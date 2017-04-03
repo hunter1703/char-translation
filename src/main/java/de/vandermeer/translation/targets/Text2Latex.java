ea83f28f456de27b47d35ef1909679bc187739a7
vdmeer <sven@vandermeer.de>
2017-04-03 23:27:57 +0100

    pushed file versions, fixed bugs

--- src/main/java/de/vandermeer/translation/targets/Text2Latex.java
+++ src/main/java/de/vandermeer/translation/targets/Text2Latex.java
@@ -24,7 +24,7 @@ import de.vandermeer.skb.interfaces.translators.TargetTranslator;
  * Target translator for source Text and target LaTeX.
  *
  * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
- * @version    v0.0.1 build 160319 (19-Mar-16) for Java 1.7
+ * @version    v0.0.1 build 170331 (31-Mar-17) for Java 1.8
  * @since      v0.0.2
  */
 public class Text2Latex implements TargetTranslator {
--- src/test/java/de/vandermeer/translation/characters/Test_CodeForDocs.java
+++ src/test/java/de/vandermeer/translation/characters/Test_CodeForDocs.java
@@ -21,7 +21,7 @@ import org.junit.Test;
  * Tests for ASCII Heading for code used in documentation.
  *
  * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
- * @version    v0.0.1 build 160319 (19-Mar-16) for Java 1.7
+ * @version    v0.0.1 build 170331 (31-Mar-17) for Java 1.8
  * @since      v0.0.2
  */
 public class Test_CodeForDocs {
